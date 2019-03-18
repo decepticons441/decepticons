f5a15a3aad6e593859b20a96cb8cbce1b7cc6b80
Nelson Tan <nestan@uw.edu>
2019-03-17 19:21:50 -0700

    Deployed but takes too long to respond need to look at index.js code

--- deployRTC/convertToAWS/deploy.sh
+++ deployRTC/convertToAWS/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-
 sh build.sh
 
 docker push nestan/webrtc
 
-ssh -i "webRTC.pem" ec2-user@ec2-3-209-165-5.compute-1.amazonaws.com 'bash-s' < upgrade-server.sh
+ssh -i "webRTC.pem" ec2-user@ec2-3-209-165-5.compute-1.amazonaws.com 'bash -s' < upgrade-server.sh
